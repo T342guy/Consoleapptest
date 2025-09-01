fc2c4ec744e807651abfcd2a6506b413d6237c86
T342 <chaircomputeroverdrive@gmail.com>
2025-08-31 17:26:58 -0700

    added ascii

--- Consoleapptest/Consoleapptest.cpp
+++ Consoleapptest/Consoleapptest.cpp
@@ -10,10 +10,24 @@ void print(string printval)
     cout << printval << endl;
 }
 
+int ascii()
+{
+    print("_____________________ ________");
+    print("___  __/__|__  /__  // /__|__ \\_______ _____  _______  __");
+    print("__  /   ___/_ < _  // /_____/ /__  __ `/_  / / /__  / / /");
+    print("_  /    ____/ / /__  __/_  __/ _  /_/ / / /_/ / _  /_/ / ");
+    print("/_/     /____/    /_/   /____/ _\\__, /  \\__,_/  _\\__, /");
+    print("                               /____/           /____/   ");
+}
+
+// remember to make sure the compiler can SEE the func BEFORE it is called, so that it doesnt complain. 
 int main()
 {
+    ascii();
     print("Hello World");
     print("this took me longer then i would have hoped for");
+    
+    
     // this allows it to stay open when compiled as an EXE, as the debug compiles it so that it will notkill after it is done. 
     system("PAUSE");
 }
